c15d06b7b7b82fbe6786e1ca9ff15cdd11a43721
Matias-vasquez <ms.vasquez@duocuc.cl>
2026-06-15 17:13:28 -0400

    Update Bebestible.java

--- ingredientes/src/main/java/banquetera/ingredientes/model/Bebestible.java
+++ ingredientes/src/main/java/banquetera/ingredientes/model/Bebestible.java
@@ -7,13 +7,17 @@ package banquetera.ingredientes.model
 @Data
 public class Bebestible(){
   @Id
-  @Column()
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @Column(name = "ID", nullable = false)
   private Long id;
 
-  @Column()
+  @NotBlank(message = "El nombre del ingrediente es obligatorio")
+  @Size(max = 50, message = "El nombre no puede superar los 50 caracteres")
+  @Column(name = "NOMBRE", nullable = false, lenght = 50)
   private String nombre;
 
-  @Column()
+  @Size(max = 200, message = "El nombre no puede superar los 200 caracteres")
+  @Column(name = "DESCRIPCION", nullable = true, lenght = 200)
   private String descripcion;
 
 }
